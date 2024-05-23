268683b432a2f61ff02b6c2a65ed0ebe96ed6fa3
mwunnam <mwunnam@gmail.com>
2024-05-23 16:21:59 +0100

    done with some basic html, css and flask

---
+++ web_flask/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Flask app for Anti-Obesity App
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
